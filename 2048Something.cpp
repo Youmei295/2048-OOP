8c4b276cf23976d4ecc02fb8f0a5fc05b55af113
Tran Tan Phat <24120118@student.hcmus.edu.vn>
2025-07-15 20:37:02 +0700

    Update 2048Something.cpp
    Add only spamtile after moving setting

--- 2048Something.cpp
+++ 2048Something.cpp
@@ -5,17 +5,26 @@ int main()
 	srand(time(nullptr));
 	Board playTable;
 	char direction;
-	playTable.spamTile();
-	playTable.spamTile();
+	int firstX = rand() % 4, firstY = rand() % 4;
+	playTable.table[firstX][firstY]=new Tile(2);
+	int secondX = rand() % 4, secondY = rand() % 4;
+	while (secondX == firstX && secondY == firstY)
+	{
+		secondX = rand() % 4;
+		secondY = rand() % 4;
+	}
+	playTable.table[secondX][secondY] = new Tile(2);
 	playTable.simpleDisplay();
 	while (!playTable.checkLose())
 	{
 		direction=_getch();
-		playTable.move(direction);
+		if (playTable.move(direction))
+		{
+			playTable.spamTile();
+		}
 		if (playTable.checkWin())
 			return 0;
 		playTable.resetMergeFlag();
-		playTable.spamTile();
 		playTable.simpleDisplay();
 		cout << endl;
 	}
